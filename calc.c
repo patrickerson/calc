c32bd18b7518539234108cd9f4199ee3937cfeee
patrickerson <add>
2020-04-10 21:38:36 -0300

    Conversions converted to a global conversor

--- calc.c
+++ calc.c
@@ -2,136 +2,93 @@
 #include <math.h>
 
 
-char hex[16] = {
+char hex[16] =  {
     '0', '1', '2', '3', '4', '5', '6', '7', 
-    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+    };
 
 char octal[8] = {
-    '0', '1', '2', '3', '4', '5', '6', '7'};
+    '0', '1', '2', '3', '4', '5', '6', '7'
+    };
 
-int number_bits  = 8;
-int hex_digits   = 2;
-int octal_digits = 3;
+char bin[2] =   {
+    '0', '1'    
+    };
 
-void dec_bin(int value, int* result){
-    int pos = number_bits;
-    
-    for(int i=0;i<number_bits;i++){
-        result[i] = 0;
+int number_bits = 8;   int hex_digits = 2;   int octal_digits = 3;
+
+
+
+void global_from_dec_conversor(
+    int dec_value, 
+    char* table, 
+    int digits, 
+    int size_table, 
+    char* result){
+
+    double acc = dec_value;
+    for(int i=digits-1; i>=0;i--){
+        int int_acc;
+        acc = acc / size_table;
+        int_acc = acc;
+        int pos = (acc - int_acc) * size_table;
+        result[i] = table[pos];
     }
-     printf("\n");
-    while(value > 0){
-        
-        pos--;
-        if((value % 2) == 0){
-            result[pos] = 0;
-        }else{
-            result[pos] = 1;
-        }
-        value = value/2;
-       
-    }
-
-
-
 }
 
-int bin_dec(int value[number_bits]){
-    double mult;
-    double pos;
-    int result;
-    for(int i =0; i<number_bits;i++){
-      int acc;
-        if( value[i] == 1){
-            pos = number_bits -i -1;
-            mult = pow(2, pos);
-            acc +=mult;
-            
-        }
-     
-      result = acc;
-    }
-    return result;
 
-    
+int global_to_dec_conversor(char* array, int digits, char* table, int size_table){
+    int acc = 0;
+        for(int i=0; i<digits;i++){
+            for(int pos = 0; pos<size_table;pos++){
+                if(array[i] == table[pos]){
+                    double value =  pos * pow(size_table, digits -1 -i);
+                    acc += value;
+                }
+            }
+        }
+        return acc;
+}
+
+void dec_bin(int value, char* result){
+    global_from_dec_conversor(value, bin, number_bits, 2, result);
+}
+
+int bin_dec(char* array){
+    return global_to_dec_conversor(array, number_bits, bin, 2);
 }
 
 void dec_hex(int decimal, char* result){
-    double acc = decimal;
-    
-    for(int i=hex_digits-1; i>=0;i--){
-        int int_acc;
-        acc = acc / 16;
-        int_acc = acc;
-        int pos = (acc - int_acc) * 16;
-        result[i] = hex[pos];
-
-
-    }
-
+    global_from_dec_conversor(decimal, hex, hex_digits, 16, result);
 }
 
 
 int hex_dec(char* array){
-    int acc = 0;
-    for(int i=0; i<hex_digits;i++){
-        for(int pos_hex = 0; pos_hex<16;pos_hex++){
-            
-            if(array[i] == hex[pos_hex]){
-                double value =  pos_hex * pow(16, hex_digits -1 -i);
-                acc += value;
-               
-            }
-        }
-    }
-    return acc;
-
+    return global_to_dec_conversor(array, hex_digits, hex, 16);
 }
+
 
 void dec_octal(int decimal, char* result){
-    double acc = decimal;
-
-    for(int i=octal_digits -1; i>=0;i--){
-        int int_acc;
-
-        acc = acc / 8;
-        int_acc = acc;
-        int pos = (acc - int_acc) * 8;
-        result[i] = octal[pos];
-
-    }
+    global_from_dec_conversor(decimal, octal, octal_digits, 8, result);
 }
+
 
 int octal_dec(char* array){
-    int acc = 0;
-    for(int i=0; i<octal_digits;i++){
-        for(int pos_octal = 0; pos_octal<8;pos_octal++){
-            
-            if(array[i] == hex[pos_octal]){
-                double value =  pos_octal * pow(8, octal_digits -1 -i);
-                acc += value;
-               
-            }
-        }
-    }
-    return acc;
+    return global_to_dec_conversor(array, octal_digits, octal, 8);
 }
-
 
 
 void main(){
-    int array[number_bits];
+    char array[number_bits];
     dec_bin(200, array);
     
-    char hex_array[2];
+    char hex_array[hex_digits];
     dec_hex(bin_dec(array), hex_array);
     hex_dec(hex_array);
-
-    char octal_array[3];
+    
+    char octal_array[octal_digits];
     dec_octal(hex_dec(hex_array), octal_array);
-
-    for(int i=0;i<3;i++){
-        printf("%c", octal_array[i]);
+    for(int i=0; i<number_bits; i++){
+    printf("%c", array[i]);
     }
-      printf("\n%d\n", octal_dec(octal_array));
 }
